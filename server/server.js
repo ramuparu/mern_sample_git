ded0a4691acc203b8d7519f073928474f9bbc482
ramuparu <mallaramuvenkatesh00007@gmail.com>
2023-03-18 12:43:20 +0530

    lesson_1_server_setup

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+const PORT = process.env.PORT|| 3500
+
+app.use('/',express.static(path.join(__dirname,'public')))
+
+app.use('/',require('./routes/routes.js'))
+
+app.all('*',(req,res)=>{
+    res.status(404)
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname,'views','404.html'))
+    }else if(req.accepts('json')){
+        res.json({message : "not found"})
+    }else{
+        res.type('txt').send('404 not found')
+    }
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server Running On Port : ${PORT}`)
+})
