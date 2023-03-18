dea91b19c768ff460c0cb059b95437dd77bb3a3d
ramuparu <mallaramuvenkatesh00007@gmail.com>
2023-03-18 14:00:28 +0530

    logger_middleware_added

---
+++ server/middlewares/logger.js
@@ -0,0 +1,30 @@
+const {format} = require('date-fns')
+
+const {v4:uuid} = require('uuid')
+
+const path = require('path')
+
+const fsPromises = require('fs').promises
+
+const fs = require('fs')
+
+const logEvents = async (message,logFileName)=>{
+    const dateformat = `${format(new Date(),'yyyyMMdd\tHH:MI:SS')}`
+    const logItem = `${dateformat}\t${uuid}\t${message}\n`
+    try {
+        if(!fs.existsSync(path.join(__dirname,'..','logs'))){
+            await fsPromises.mkdir(path.join(__dirname,'..','logs'))
+        }
+        await fsPromises.appendFile(path.join(__dirname,'..','logs',logFileName),logItem)
+    } catch (error) {
+        console.log(error)
+    } 
+}
+
+const logger = (req,res,next)=>{
+    logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`,'reqLog.log')
+    console.log(`${req.method}\t${req.path}`)
+    next()
+}
+
+module.exports = logger
--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const path = require('path')
+const logger = require('./middlewares/logger.js')
 const app = express()
 
+
+
 const PORT = process.env.PORT|| 3500
+
+app.use(logger)
+
+app.use(express.json())
 
 app.use('/',express.static(path.join(__dirname,'public')))
 
